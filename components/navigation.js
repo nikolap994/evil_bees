2b97eb6b2a57c3f9a787d655d9c5db5e1a6607e9
Nikola Pavlović <nikolap994@gmail.com>
2022-10-23 16:13:28 +0200

    Add basic fetching

--- components/navigation.js
+++ components/navigation.js
@@ -1,22 +1,30 @@
 import Link from "next/link";
 
 export default function Navigation() {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link href="/about">About us</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact us</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+	return (
+		<nav>
+			<ul>
+				<li>
+					<Link href="/">Home</Link>
+				</li>
+				<li>
+					<Link href="/projects">Projects</Link>
+				</li>
+				<li>
+					<Link href="/about">About us</Link>
+				</li>
+				<ul>
+					<li>
+						<Link href="/members">Members</Link>
+					</li>
+					<li>
+						<Link href="/members/hall-of-fame">Hall of Fame</Link>
+					</li>
+				</ul>
+				<li>
+					<Link href="/contact">Contact us</Link>
+				</li>
+			</ul>
+		</nav>
+	);
 }
