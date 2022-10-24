7f8dbfff542be10adfe7a5116790bdcfd2d45027
Nikola Pavlović <nikolap994@gmail.com>
2022-10-24 19:09:18 +0200

    Add evil bees logo

--- components/navigation.js
+++ components/navigation.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Navigation() {
@@ -9,9 +10,16 @@ export default function Navigation() {
 			<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 				<nav className="relative z-50 flex justify-between">
 					<div className="flex items-center md:gap-x-12">
-						<Link href="/">Home</Link>
-						<Link href="/projects">Projects</Link>
+						<Link href="/">
+							<Image
+								width="100"
+								height="100"
+								alt="evil-bees"
+								src="/evil-bees.png"
+							/>
+						</Link>
 						<Link href="/members">Members</Link>
+						<Link href="/projects">Projects</Link>
 						<Link href="/contact">Contact us</Link>
 					</div>
 					<button onClick={() => signIn()}>Sign in</button>
