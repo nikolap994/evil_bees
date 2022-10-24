07dfc50f06057497f3b08eebc19064fe02484ca5
Nikola Pavlović <nikolap994@gmail.com>
2022-10-24 19:01:42 +0200

    Add github in footer

--- components/footer.js
+++ components/footer.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Footer() {
 	return (
@@ -6,7 +7,9 @@ export default function Footer() {
 			<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 				<div className="flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-between">
 					<div className="flex gap-x-6">
-						<Link href="https://github.com/Evil-Bees">Github</Link>
+						<Link href="https://github.com/Evil-Bees">
+							<Image width="32" height="32" alt="github" src="/github.png" />
+						</Link>
 					</div>
 					<div className="mt-6 text-sm text-slate-500 sm:mt-0">Evil Bees</div>
 				</div>
