b51462b407be6de7dc172ee6be5218748fda1df9
Marija Cehic <cehicmarija@gmail.com>
2022-10-26 22:25:33 +0200

    Add Hero Image to Homepage Hero

--- components/hero/homepage-hero.js
+++ components/hero/homepage-hero.js
@@ -1,12 +1,24 @@
+import Image from "next/image";
+import growthImage from "../../public/images/growth-2.png";
+
 export default function HomepageHero() {
 	return (
-		<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-20 pb-16 text-center lg:pt-32">
-			<h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
-				Lack the drive to level up as a developer? You’re in the right place!
-			</h1>
-			<p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
-				Make learning, levelling up fun and interactive.
-			</p>
-		</div>
+		<section className="relative">
+			<div className="absolute right-0 bottom-0 z-[-1]">
+				<Image
+					src={growthImage}
+					alt="Man with purple hair flying on a rocket"
+					className=""
+				/>
+			</div>
+			<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-20 pb-16 text-center lg:pt-20">
+				<h1 className="z-10 max-w-4xl font-display text-5xl font-medium tracking-tight text-left text-slate-900 sm:text-7xl">
+					Lack the drive to level up as a developer? You’re in the right place!
+				</h1>
+				<p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
+					Make learning, levelling up fun and interactive.
+				</p>
+			</div>
+		</section>
 	);
 }
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@ import OpenSourceFAQ from "../components/cta/open-source-faq";
 import HardTruthCTA from "../components/cta/hard-truth";
 import StandForCTA from "../components/cta/what-we-stand-for";
 import SolvingProblems from "../components/cta/solving-problems";
+import HomepageHero from "../components/hero/homepage-hero";
 
 export default function Home() {
 	return (
 		<section>
+			<HomepageHero />
 			<HardTruthCTA />
 			<OpenSourceFAQ />
 			<StandForCTA />
