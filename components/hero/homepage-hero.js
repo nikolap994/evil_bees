5823ea6e301a5f7f106c3c6a61906133b4fb32c8
Marija Cehic <cehicmarija@gmail.com>
2022-10-26 21:28:53 +0200

    Add Homepage Hero Component

---
+++ components/hero/homepage-hero.js
@@ -0,0 +1,12 @@
+export default function HomepageHero() {
+	return (
+		<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-20 pb-16 text-center lg:pt-32">
+			<h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
+				Lack the drive to level up as a developer? You’re in the right place!
+			</h1>
+			<p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
+				Make learning, levelling up fun and interactive.
+			</p>
+		</div>
+	);
+}
