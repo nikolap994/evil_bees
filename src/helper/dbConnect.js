1e6e8b18154d7b39f5739940a1fb0ec99cda3666
Nikola Pavlović <nikolap994@gmail.com>
2022-10-26 17:17:37 +0200

    Update Database connection logic

---
+++ src/helper/dbConnect.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+if (!process.env.MONGODB_URI) {
+	throw new Error("Please add your MONGODB_URI to .env.local");
+}
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+let globalWithMongoose = global;
+let cached = globalWithMongoose.mongoose;
+
+if (!cached) {
+	cached = globalWithMongoose.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+	if (cached.conn) {
+		return cached.conn;
+	}
+	if (!cached.promise) {
+		const opts = {
+			bufferCommands: false,
+		};
+		cached.promise = mongoose.connect(MONGODB_URI, opts).then(mongoose => {
+			return mongoose;
+		});
+	}
+	cached.conn = await cached.promise;
+	return cached.conn;
+}
+
+export default dbConnect;
