57d44a8724a66102c7d1b8d037d28570e5bfd692
Nikola Pavlović <nikolap994@gmail.com>
2022-10-29 21:27:58 +0200

    Add example code of conduct

---
+++ pages/code-of-conduct.js
@@ -0,0 +1,394 @@
+export default function CodeOfConduct() {
+	return (
+		<div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+			<h1 id="code-of-conduct">CODE OF CONDUCT</h1>
+			<h2 id="introduction">INTRODUCTION</h2>
+			<p>
+				This code of conduct applies to all spaces managed by the Apache
+				Software Foundation, including IRC, all public and private mailing
+				lists, issue trackers, wikis, blogs, Twitter, and any other
+				communication channel used by our communities. A code of conduct which
+				is specific to in-person events (ie., conferences) is codified in the
+				published ASF anti-harassment policy.
+			</p>
+			<p>
+				We expect this code of conduct to be honored by everyone who
+				participates in the Apache community formally or informally, or claims
+				any affiliation with the Foundation, in any Foundation-related
+				activities and especially when representing the ASF, in any role.
+			</p>
+			<p>
+				This code is <strong>not exhaustive or complete</strong>. It serves to
+				distill our common understanding of a collaborative, shared environment
+				and goals. We expect it to be followed in spirit as much as in the
+				letter, so that it can enrich all of us and the technical communities in
+				which we participate.
+			</p>
+			<h2 id="specific-guidelines">SPECIFIC GUIDELINES</h2>
+			<p>We strive to:</p>
+			<ol>
+				<li>
+					<p>
+						<strong>Be open.</strong> We invite anyone to participate in our
+						community. We preferably use public methods of communication for
+						project-related messages, unless discussing something sensitive.
+						This applies to messages for help or project-related support, too;
+						not only is a public support request much more likely to result in
+						an answer to a question, it also makes sure that any inadvertent
+						mistakes made by people answering will be more easily detected and
+						corrected.
+					</p>
+				</li>
+				<li>
+					<p>
+						<strong>Be empathetic, welcoming, friendly, and patient.</strong> We
+						work together to resolve conflict, assume good intentions, and do
+						our best to act in an empathetic fashion. We may all experience some
+						frustration from time to time, but we do not allow frustration to
+						turn into a personal attack. A community where people feel
+						uncomfortable or threatened is not a productive one. We should be
+						respectful when dealing with other community members as well as with
+						people outside our community.
+					</p>
+				</li>
+				<li>
+					<p>
+						<strong>Be collaborative.</strong> Our work will be used by other
+						people, and in turn we will depend on the work of others. When we
+						make something for the benefit of the project, we are willing to
+						explain to others how it works, so that they can build on the work
+						to make it even better. Any decision we make will affect users and
+						colleagues, and we take those consequences seriously when making
+						decisions.
+					</p>
+				</li>
+				<li>
+					<p>
+						<strong>Be inquisitive.</strong> Nobody knows everything! Asking
+						questions early avoids many problems later, so questions are
+						encouraged, though they may be directed to the appropriate forum.
+						Those who are asked should be responsive and helpful, within the
+						context of our shared goal of improving Apache project code.
+					</p>
+				</li>
+				<li>
+					<p>
+						<strong>Be careful in the words that we choose.</strong> Whether we
+						are participating as professionals or volunteers, we value
+						professionalism in all interactions, and take responsibility for our
+						own speech. Be kind to others. Do not insult or put down other
+						participants. Harassment and other exclusionary behavior are not
+						acceptable. This includes, but is not limited to:
+					</p>
+					<ul>
+						<li>
+							Violent threats or language directed against another person.
+						</li>
+						<li>
+							Sexist, racist, or otherwise discriminatory jokes and language.
+						</li>
+						<li>Posting sexually explicit or violent material.</li>
+						<li>
+							Posting (or threatening to post) other people&#39;s personally
+							identifying information (&quot;doxing&quot;).
+						</li>
+						<li>
+							Sharing private content, such as emails sent privately or
+							non-publicly, or unlogged forums such as IRC channel history.
+						</li>
+						<li>
+							Personal insults, especially those using racist or sexist terms.
+						</li>
+						<li>Unwelcome sexual attention.</li>
+						<li>Excessive or unnecessary profanity.</li>
+						<li>
+							Repeated harassment of others. In general, if someone asks you to
+							stop, then stop.
+						</li>
+						<li>Advocating for, or encouraging, any of the above behavior.</li>
+					</ul>
+				</li>
+				<li>
+					<p>
+						<strong>Be concise.</strong> Keep in mind that what you write once
+						will be read by hundreds of persons. Writing a short email means
+						people can understand the conversation as efficiently as possible.
+						Short emails should always strive to be empathetic, welcoming,
+						friendly and patient. When a long explanation is necessary, consider
+						adding a summary.
+					</p>
+					<p>
+						{" "}
+						Try to bring new ideas to a conversation so that each mail adds
+						something unique to the thread, keeping in mind that the rest of the
+						thread still contains the other messages with arguments that have
+						already been made.
+					</p>
+					<p>
+						{" "}
+						Try to stay on topic, especially in discussions that are already
+						fairly large.
+					</p>
+				</li>
+				<li>
+					<p>
+						<strong>Step down considerately.</strong> Members of every project
+						come and go. When somebody leaves or disengages from the project
+						they should tell people they are leaving and take the proper steps
+						to ensure that others can pick up where they left off. In doing so,
+						they should remain respectful of those who continue to participate
+						in the project and should not misrepresent the project&#39;s goals
+						or achievements. Likewise, community members should respect any
+						individual&#39;s choice to leave the project.
+					</p>
+				</li>
+			</ol>
+			<h2 id="diversity-statement">DIVERSITY STATEMENT</h2>
+			<p>
+				Apache welcomes and encourages participation by everyone. We are
+				committed to being a community that everyone feels good about joining.
+				Although we may not be able to satisfy everyone, we will always work to
+				treat everyone well.
+			</p>
+			<p>
+				No matter how you identify yourself or how others perceive you: we
+				welcome you. Though no list can hope to be comprehensive, we explicitly
+				honour diversity in: age, culture, ethnicity, genotype, gender identity
+				or expression, language, national origin, neurotype, phenotype,
+				political beliefs, profession, race, religion, sexual orientation,
+				socioeconomic status, subculture and technical ability.
+			</p>
+			<p>
+				Though we welcome people fluent in all languages, Apache development is
+				conducted in English.
+			</p>
+			<p>
+				Standards for behaviour in the Apache community are detailed in the Code
+				of Conduct above. We expect participants in our community to meet these
+				standards in all their interactions and to help others to do so as well.
+			</p>
+			<h2 id="reporting-guidelines">REPORTING GUIDELINES</h2>
+			<p>
+				While this code of conduct should be adhered to by participants, we
+				recognize that sometimes people may have a bad day, or be unaware of
+				some of the guidelines in this code of conduct. When that happens, you
+				may reply to them and point out this code of conduct. Such messages may
+				be in public or in private, whatever is most appropriate. However,
+				regardless of whether the message is public or not, it should still
+				adhere to the relevant parts of this code of conduct; in particular, it
+				should not be abusive or disrespectful.
+			</p>
+			<p>
+				If you believe someone is violating this code of conduct, you may reply
+				to them and point out this code of conduct. Such messages may be in
+				public or in private, whatever is most appropriate. Assume good faith;
+				it is more likely that participants are unaware of their bad behaviour
+				than that they intentionally try to degrade the quality of the
+				discussion. Should there be difficulties in dealing with the situation,
+				you may report your compliance issues in confidence to either:
+			</p>
+			<ul>
+				<li>
+					President of the Apache Software Foundation: Sam Ruby (rubys at
+					intertwingly dot net)
+				</li>
+			</ul>
+			<p>Or one of our volunteers:</p>
+			<ul>
+				<li>
+					<a href="http://home.apache.org/~markt/coc.html">Mark Thomas</a>
+				</li>
+				<li>
+					<a href="http://home.apache.org/~wohali/">Joan Touzet</a>
+				</li>
+				<li>
+					<a href="http://home.apache.org/~sharan/coc.html">Sharan Foga</a>
+				</li>
+			</ul>
+			<p>
+				If the violation is in documentation or code, for example inappropriate
+				pronoun usage or word choice within official documentation, we ask that
+				people report these privately to the project in question at{" "}
+				<a href="&#x6d;&#97;&#x69;&#x6c;&#116;&#x6f;&#58;&#112;&#114;&#x69;&#x76;&#97;&#116;&#101;&#64;&#112;&#114;&#111;&#x6a;&#x65;&#x63;&#116;&#46;&#x61;&#112;&#97;&#99;&#x68;&#x65;&#x2e;&#111;&#114;&#103;">
+					&#112;&#114;&#x69;&#x76;&#97;&#116;&#101;&#64;&#112;&#114;&#111;&#x6a;&#x65;&#x63;&#116;&#46;&#x61;&#112;&#97;&#99;&#x68;&#x65;&#x2e;&#111;&#114;&#103;
+				</a>
+				, and, if they have sufficient ability within the project, to resolve or
+				remove the concerning material, being mindful of the perspective of the
+				person originally reporting the issue.
+			</p>
+			<h2 id="endnotes">ENDNOTES</h2>
+			<p>
+				This Code defines <strong>empathy</strong> as &quot;a vicarious
+				participation in the emotions, ideas, or opinions of others; the ability
+				to imagine oneself in the condition or predicament of another.&quot;{" "}
+				<strong>Empathetic</strong> is the adjectival form of empathy.
+			</p>
+			<p>
+				This statement thanks the following, on which it draws for content and
+				inspiration:
+			</p>
+			<ul>
+				<li>
+					<a href="http://couchdb.apache.org/conduct.html">
+						CouchDB Project Code of conduct
+					</a>
+				</li>
+				<li>
+					<a href="http://fedoraproject.org/code-of-conduct">
+						Fedora Project Code of Conduct
+					</a>
+				</li>
+				<li>
+					<a href="http://speakup.io/coc.html">Speak Up! Code of Conduct</a>
+				</li>
+				<li>
+					<a href="https://www.djangoproject.com/conduct/">
+						Django Code of Conduct
+					</a>
+				</li>
+				<li>
+					<a href="http://www.debian.org/vote/2014/vote_002">
+						Debian Code of Conduct
+					</a>
+				</li>
+				<li>
+					<a href="https://github.com/twitter/code-of-conduct/blob/master/code-of-conduct.md">
+						Twitter Open Source Code of Conduct
+					</a>
+				</li>
+				<li>
+					<a href="https://wiki.mozilla.org/Code_of_Conduct/Draft#Conflicts_of_Interest">
+						Mozilla Code of Conduct/Draft
+					</a>
+				</li>
+				<li>
+					<a href="https://www.python.org/community/diversity/">
+						Python Diversity Appendix
+					</a>
+				</li>
+				<li>
+					<a href="http://pythonmentors.com">Python Mentors Home Page</a>
+				</li>
+			</ul>
+			<h1 id="slack-community-guidelines">Slack Community Guidelines</h1>
+			<p>
+				If you decide to join the{" "}
+				<a href="https://join.slack.com/t/apache-superset/shared_invite/zt-16jvzmoi8-sI7jKWp~xc2zYRe~NqiY9Q">
+					Community Slack
+				</a>
+				, please adhere to the following rules:
+			</p>
+			<p>
+				<strong>1. Treat everyone in the community with respect.</strong>
+			</p>
+			<ul>
+				<li>
+					We strive to make this community a warm place for people from all
+					industries, use cases, geographies, and backgrounds. Harassment of any
+					kind is not acceptable and won’t be tolerated.
+				</li>
+				<li>
+					Please follow the guidelines as outlined in the Superset Community{" "}
+					<a href="https://github.com/apache/superset/blob/master/CODE_OF_CONDUCT.md">
+						code of conduct here
+					</a>
+					.
+				</li>
+			</ul>
+			<p>
+				<strong>2. Use the right channel.</strong>
+			</p>
+			<ul>
+				<li>
+					Channels are an effective way to organize and focus discussions while
+					also empowering members to opt-in to the types of content they’re
+					interested in. When questions are posted or discussions are started in
+					the wrong channel, it dilutes the trust of the members in the channel
+					and, more practically, makes it harder for your questions to be
+					answered.
+				</li>
+			</ul>
+			<p>
+				<strong>3. Ask thoughtful questions.</strong>
+			</p>
+			<ul>
+				<li>
+					<p>
+						We’re all here to help each other out. The best way to get help is
+						by investing effort into your questions. First check and see if your
+						question is answered in{" "}
+						<a href="https://superset.apache.org/faq.html">
+							the Superset documentation
+						</a>{" "}
+						or on{" "}
+						<a href="https://stackoverflow.com/search?q=apache+superset">
+							Stack Overflow
+						</a>
+						. You can also check{" "}
+						<a href="https://github.com/apache/superset/issues">
+							GitHub issues
+						</a>{" "}
+						to see if your question or feature request has been submitted
+						before. Then, use Slack search to see if your question has already
+						been asked and answered in the past. If you still feel the need to
+						ask a question, make sure you include:
+					</p>
+				</li>
+				<li>
+					<p>The steps you’ve already taken</p>
+				</li>
+				<li>
+					Relevant details presented cleanly (text stacktraces, formatted
+					markdown, or screenshots. Please don’t paste large blocks of code
+					unformatted or post photos of your screen from your phone)
+				</li>
+				<li>
+					The specific question you have or the specific type of help you&#39;re
+					seeking
+				</li>
+			</ul>
+			<p>
+				<strong>4. Avoid double posting</strong>
+			</p>
+			<ul>
+				<li>
+					This Slack community is not a customer support channel and all members
+					are here voluntarily. If you aren’t getting a response to a question
+					you have, make sure you look at rules 1, 2, and 3. It’s also worth
+					remembering that there may not be someone in the community who has the
+					context to help you out.
+				</li>
+			</ul>
+			<p>
+				<strong>5. Communicate openly</strong>
+			</p>
+			<ul>
+				<li>
+					Unless you have explicit permission from the person, please avoid
+					sending direct messages to individuals. Communicating in public
+					channels ensures that we’re all respecting each other’s attentions and
+					we can scalably moderate our communication to mitigate harassment or
+					discrimination. Do not use direct messages to pitch products and
+					services. If you are receiving unwelcome direct messages, please
+					notify an admin.
+				</li>
+			</ul>
+			<p>
+				<strong>
+					6. Practice good Slack hygiene by using threads for discussions and
+					emojis for light reactions.
+				</strong>
+			</p>
+			<ul>
+				<li>
+					The medium is the message. Slack can foster a warm, collaborative, and
+					organized community when used effectively. We want to respect people’s
+					attentions (thread notifications &gt; channel notifications &gt; DM
+					notifications) and we want to improve information density (a member
+					should be able to browse and explore many convo threads, not just see
+					one thread discussed in a top level channel).
+				</li>
+			</ul>
+		</div>
+	);
+}
