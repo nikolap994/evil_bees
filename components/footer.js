fe5086c6058e24c351966102eaa626fd6307715f
Nikola Pavlović <nikolap994@gmail.com>
2022-10-29 21:15:26 +0200

    Update footer sticky to bottom

--- components/footer.js
+++ components/footer.js
@@ -3,15 +3,25 @@ import Image from "next/image";
 
 export default function Footer() {
 	return (
-		<footer className="bg-slate-50">
+		<footer className="relative z-50 bg-indigo-900 mt-auto">
 			<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 				<div className="flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-between">
 					<div className="flex gap-x-6">
 						<Link href="https://github.com/Evil-Bees">
-							<Image width="32" height="32" alt="github" src="/github.png" />
+							<Image width="80" height="80" alt="github" src="/github.png" />
 						</Link>
 					</div>
-					<div className="mt-6 text-sm text-slate-500 sm:mt-0">Evil Bees</div>
+					<div className="mt-6 text-sm text-slate-500 sm:mt-0">
+						<Link href="/">
+							<Image
+								priority={true}
+								width="100"
+								height="100"
+								alt="evil-bees"
+								src="/evil-bees.png"
+							/>
+						</Link>
+					</div>
 				</div>
 			</div>
 		</footer>
