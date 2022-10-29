928520d6918734028f4f2e224cd1ecb50d5ad762
Nikola Pavlović <nikolap994@gmail.com>
2022-10-29 23:06:46 +0200

    Update navbar styles

--- components/navigation.js
+++ components/navigation.js
@@ -2,12 +2,11 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function Navigation() {
-
 	return (
 		<header className="bg-indigo-900 mb-10">
 			<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8  text-white">
-				<nav className="relative z-50 flex justify-between">
-					<div className="flex items-center md:gap-x-12">
+				<nav className="flex items-center justify-between flex-wrap p-6">
+					<div className="flex items-center flex-shrink-0 text-white mr-6">
 						<Link href="/">
 							<Image
 								priority={true}
@@ -17,12 +16,41 @@ export default function Navigation() {
 								src="/evil-bees.png"
 							/>
 						</Link>
-						<Link href="/projects">Projects</Link>
-						<Link href="/our-team">Our Team</Link>
-						<Link href="/code-of-conduct">Code of Conduct</Link>
-						<Link href="/contact">Contact us</Link>
 					</div>
-					<Link className="flex items-center md:gap-x-12" href="https://discord.gg/rvxGNrFhNz">Join Us!</Link>
+					<div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+						<div className="text-l lg:flex-grow">
+							<Link
+								className="block mt-4 lg:inline-block lg:mt-0 text-white-400 hover:text-white mr-4"
+								href="/projects"
+							>
+								Projects
+							</Link>
+							<Link
+								className="block mt-4 lg:inline-block lg:mt-0 text-white-400 hover:text-white mr-4"
+								href="/our-team"
+							>
+								Our Team
+							</Link>
+							<Link
+								className="block mt-4 lg:inline-block lg:mt-0 text-white-400 hover:text-white mr-4"
+								href="/code-of-conduct"
+							>
+								Code of Conduct
+							</Link>
+							<Link
+								className="block mt-4 lg:inline-block lg:mt-0 text-white-400 hover:text-white mr-4"
+								href="/contact"
+							>
+								Contact us
+							</Link>
+						</div>
+					</div>
+					<Link
+						className="inline-block text-l px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-black hover:bg-white mt-4 lg:mt-0"
+						href="https://discord.gg/rvxGNrFhNz"
+					>
+						Join Us!
+					</Link>
 				</nav>
 			</div>
 		</header>
