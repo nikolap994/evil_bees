23ef71c7ad17c74cf7d6144ba2729cf7b29814f5
Nikola Pavlović <nikolap994@gmail.com>
2022-10-29 21:15:32 +0200

    Update layout class config

--- components/layout.js
+++ components/layout.js
@@ -2,11 +2,11 @@ import Navigation from "./Navigation";
 import Footer from "./footer";
 
 export default function Layout({ children }) {
-  return (
-    <div className="flex h-full flex-col">
-      <Navigation />
-      <main>{children}</main>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="flex flex-col min-h-screen">
+			<Navigation />
+			<main>{children}</main>
+			<Footer />
+		</div>
+	);
 }
