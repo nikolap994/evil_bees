db17709739ae719d8d8b0ac36608da2918e4be89
Nikola Pavlović <nikolap994@gmail.com>
2022-10-30 14:18:18 +0100

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import { GoogleAnalytics } from "nextjs-google-analytics";
+
 import Layout from "../components/layout";
 
 import "../styles/globals.css";
@@ -5,6 +7,7 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps: pageProps }) {
 	return (
 		<Layout>
+			<GoogleAnalytics trackPageViews />
 			<Component {...pageProps} />
 		</Layout>
 	);
