c03200fbbad60e6741001f8649dee9628da341bf
Nikola Pavlović <nikolap994@gmail.com>
2022-10-30 14:30:52 +0100

    Add cookie consent

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,47 @@
+import { useEffect } from "react";
 import { GoogleAnalytics } from "nextjs-google-analytics";
+import CookieConsent, {
+	getCookieConsentValue,
+	Cookies,
+} from "react-cookie-consent";
 
 import Layout from "../components/layout";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps: pageProps }) {
+	const handleAcceptCookie = () => {
+		<GoogleAnalytics trackPageViews />;
+	};
+
+	const handleDeclineCookie = () => {
+		Cookies.remove("_ga");
+		Cookies.remove("_gat");
+		Cookies.remove("_gid");
+	};
+
+	useEffect(() => {
+		const isConsent = getCookieConsentValue();
+		if (isConsent === "true") {
+			handleAcceptCookie();
+		}
+	}, []);
+
 	return (
 		<Layout>
-			<GoogleAnalytics trackPageViews />
 			<Component {...pageProps} />
+			<CookieConsent
+				enableDeclineButton
+				onAccept={handleAcceptCookie}
+				onDecline={handleDeclineCookie}
+			>
+				This website uses cookies to enhance the user experience.
+			</CookieConsent>
+			{getCookieConsentValue() == "true" ? (
+				<GoogleAnalytics trackPageViews />
+			) : (
+				""
+			)}
 		</Layout>
 	);
 }
