e69ba314c541a484fe604a37cc6a05ec0de3ef2a
Marija Cehic <cehicmarija@gmail.com>
2022-11-08 01:11:11 +0100

    Update Code of Conduct text/formatting

--- pages/code-of-conduct.js
+++ pages/code-of-conduct.js
@@ -12,15 +12,15 @@ export default function CodeOfConduct() {
 			</h2>
 			<br />
 			<p>
-				This code of conduct applies to all spaces managed by the Evill Bees,
+				This code of conduct applies to all spaces managed by the Evill Bees, including but not limited to:
 				all public and private mailing lists, issue trackers, wikis, blogs,
-				Twitter, and any other communication channel used by our communities. A
+				Twitter; as well as any other communication channel used by our communities. A
 				code of conduct which is specific to in-person events (ie., conferences)
 				is codified in the published ASF anti-harassment policy. We expect this
-				code of conduct to be honored by everyone who participates in the Evil
-				Bees community formally or informally, or claims any affiliation with
+				code of conduct to be honored by all who participate in the Evil
+				Bees community formally or informally, or claim any affiliation with
 				the community, in any community-related activities and especially when
-				representing the ASF, in any role. This code is{" "}
+				representing the Evil Bees, in any role. This code is{" "}
 				<strong>not exhaustive or complete</strong>. It serves to distill our
 				common understanding of a collaborative, shared environment and goals.
 				We expect it to be followed in spirit as much as in the letter, so that
@@ -42,8 +42,8 @@ export default function CodeOfConduct() {
 						<strong>Be open.</strong> We invite anyone to participate in our
 						community. We preferably use public methods of communication for
 						project-related messages, unless discussing something sensitive.
-						This applies to messages for help or project-related support, too;
-						not only is a public support request much more likely to result in
+						This applies to messages regarding help or project-related support, too.
+						Not only is a public support request much more likely to result in
 						an answer to a question, it also makes sure that any inadvertent
 						mistakes made by people answering will be more easily detected and
 						corrected.
@@ -54,18 +54,17 @@ export default function CodeOfConduct() {
 						<strong>Be empathetic, welcoming, friendly, and patient.</strong> We
 						work together to resolve conflict, assume good intentions, and do
 						our best to act in an empathetic fashion. We may all experience some
-						frustration from time to time, but we do not allow frustration to
-						turn into a personal attack. A community where people feel
-						uncomfortable or threatened is not a productive one. We should be
-						respectful when dealing with other community members as well as with
-						people outside our community.
+						frustration from time to time, but we can not allow frustration to
+						turn into any sort of personal attack. After all, a community where people feel
+						uncomfortable or threatened is not a productive one. It's imperitive for all to remain
+						respectful when dealing with other community members as well as those outside of our community.
 					</p>
 				</li>
 				<li>
 					<p>
 						<strong>Be collaborative.</strong> Our work will be used by other
 						people, and in turn we will depend on the work of others. When we
-						make something for the benefit of the project, we are willing to
+						make something for the benefit of the project, we must be willing to
 						explain to others how it works, so that they can build on the work
 						to make it even better. Any decision we make will affect users and
 						colleagues, and we take those consequences seriously when making
@@ -75,20 +74,20 @@ export default function CodeOfConduct() {
 				<li>
 					<p>
 						<strong>Be inquisitive.</strong> Nobody knows everything! Asking
-						questions early avoids many problems later, so questions are
-						encouraged, though they may be directed to the appropriate forum.
+						questions early on avoids many problems later. Thus, questions are
+						encouraged, although they ought to be directed to the appropriate forum.
 						Those who are asked should be responsive and helpful, within the
-						context of our shared goal of improving Evil Bees projects code.
+						context of our shared goal of improving the Evil Bees codebase.
 					</p>
 				</li>
 				<li>
 					<p>
-						<strong>Be careful in the words that we choose.</strong> Whether we
-						are participating as professionals or volunteers, we value
+						<strong>Be careful in with words you choose to use.</strong> Whether you are
+						are participating as a professional or a volunteer, we value
 						professionalism in all interactions, and take responsibility for our
-						own speech. Be kind to others. Do not insult or put down other
+						own words. Be kind to others. Never insult or put down other
 						participants. Harassment and other exclusionary behavior are not
-						acceptable. This includes, but is not limited to:
+						acceptable nor will they be tolerated. This includes, but is not limited to:
 					</p>
 					<ul className="list-inside list-disc">
 						<li>
@@ -100,30 +99,30 @@ export default function CodeOfConduct() {
 						<li>Posting sexually explicit or violent material.</li>
 						<li>
 							Posting (or threatening to post) other people&#39;s personally
-							identifying information (&quot;doxing&quot;).
+							identifying information (&quot;aka doxing&quot;).
 						</li>
 						<li>
 							Sharing private content, such as emails sent privately or
 							non-publicly, or unlogged forums such as IRC channel history.
 						</li>
 						<li>
-							Personal insults, especially those using racist or sexist terms.
+							Personal insults, especially those with racist or sexist undertones.
 						</li>
 						<li>Unwelcome sexual attention.</li>
-						<li>Excessive or unnecessary profanity.</li>
+						<li>Excessive or unnecessary use of profanities.</li>
 						<li>
 							Repeated harassment of others. In general, if someone asks you to
 							stop, then stop.
 						</li>
-						<li>Advocating for, or encouraging, any of the above behavior.</li>
+						<li>Advocating for, or encouraging, any of the aforementioned behavior or language.</li>
 					</ul>
 				</li>
 				<li>
 					<p>
 						<strong>Be concise.</strong> Keep in mind that what you write once
-						will be read by hundreds of persons. Writing a short email means
+						will be read by hundreds of people. Writing a short email means
 						people can understand the conversation as efficiently as possible.
-						Short emails should always strive to be empathetic, welcoming,
+						Short emails should also strive to be empathetic, welcoming,
 						friendly and patient. When a long explanation is necessary, consider
 						adding a summary. Try to bring new ideas to a conversation so that
 						each mail adds something unique to the thread, keeping in mind that
@@ -152,10 +151,10 @@ export default function CodeOfConduct() {
 			<br />
 			<p>
 				Evil Bees welcomes and encourages participation by everyone. We are
-				committed to being a community that everyone feels good about joining.
+				committed to being a community that consists of lighthearted jokes and positive interactions.
 				Although we may not be able to satisfy everyone, we will always work to
-				treat everyone well. No matter how you identify yourself or how others
-				perceive you: we welcome you. Though no list can hope to be
+				treat everyone as best we can. No matter how you identify yourself or how others
+				perceive you - we welcome you. Though no list can hope to be
 				comprehensive, we explicitly honour diversity in: age, culture,
 				ethnicity, genotype, gender identity or expression, language, national
 				origin, neurotype, phenotype, political beliefs, profession, race,
@@ -164,7 +163,7 @@ export default function CodeOfConduct() {
 				Bees development is conducted in English. Standards for behaviour in the
 				Evil Bees community are detailed in the Code of Conduct above. We expect
 				participants in our community to meet these standards in all their
-				interactions and to help others to do so as well.
+				interactions and to encourage others to do so as well.
 			</p>
 			<br />
 			<h2 className="z-10 max-w-4xl font-display text-l font-medium tracking-tight text-left text-slate-900 sm:text-l">
@@ -175,18 +174,16 @@ export default function CodeOfConduct() {
 				While this code of conduct should be adhered to by participants, we
 				recognize that sometimes people may have a bad day, or be unaware of
 				some of the guidelines in this code of conduct. When that happens, you
-				may reply to them and point out this code of conduct. Such messages may
-				be in public or in private, whatever is most appropriate. However,
-				regardless of whether the message is public or not, it should still
+				may message them in order to point out this code of conduct. Such messages may
+				be in public or in private, whatever feels most appropriate. However,
+				regardless of whether the message is posted publicly or not, it should still
 				adhere to the relevant parts of this code of conduct; in particular, it
 				should not be abusive or disrespectful.
 			</p>
 			<p>
-				If you believe someone is violating this code of conduct, you may reply
-				to them and point out this code of conduct. Such messages may be in
-				public or in private, whatever is most appropriate. Assume good faith;
-				it is more likely that participants are unaware of their bad behaviour
-				than that they intentionally try to degrade the quality of the
+				Finally, assume good faith.
+				It is more likely that participants are unaware of their bad behaviour
+				and are not intentionally trying to degrade the quality of the
 				discussion. Should there be difficulties in dealing with the situation,
 				you may report your compliance issues in confidence to either:
 			</p>
@@ -209,7 +206,6 @@ export default function CodeOfConduct() {
 				This Code defines <strong>empathy</strong> as &quot;a vicarious
 				participation in the emotions, ideas, or opinions of others; the ability
 				to imagine oneself in the condition or predicament of another.&quot;{" "}
-				<strong>Empathetic</strong> is the adjectival form of empathy.
 			</p>
 			<br />
 			<h1 className="z-10 max-w-4xl font-display text-l font-medium tracking-tight text-left text-slate-900 sm:text-l">
@@ -226,16 +222,16 @@ export default function CodeOfConduct() {
 				<strong>1. Treat everyone in the community with respect.</strong>
 			</p>
 			<p>
-				We strive to make this community a warm place for people from all
-				industries, use cases, geographies, and backgrounds. Harassment of any
+				We strive to make this community a welcoming place for people - from all
+				industries, use cases, geographies, backgrounds and walks of life. Harassment of any
 				kind is not acceptable and won’t be tolerated.
 			</p>
 			<br />
 			<p>
-				<strong>2. Use the right channel.</strong>
+				<strong>2. Use the right channel</strong>
 			</p>
 			<p>
-				Channels are an effective way to organize and focus discussions while
+				Channels are an effective way for organizing and focusing discussions while
 				also empowering members to opt-in to the types of content they’re
 				interested in. When questions are posted or discussions are started in
 				the wrong channel, it dilutes the trust of the members in the channel
@@ -244,14 +240,14 @@ export default function CodeOfConduct() {
 			</p>
 			<br />
 			<p>
-				<strong>3. Ask thoughtful questions.</strong>
+				<strong>3. Ask thoughtful questions</strong>
 			</p>
 			<ul>
 				<li>
 					<p>
 						We’re all here to help each other out. The best way to get help is
 						by investing effort into your questions. First check and see if your
-						question is answered in Github Wiki, Discord Channels or in READMEs.
+						question is answered in Github Wiki, Discord Channels or in the project's README.
 						Then, use Discord search to see if your question has already been
 						asked and answered in the past. If you still feel the need to ask a
 						question, make sure you include:
@@ -262,11 +258,11 @@ export default function CodeOfConduct() {
 					<li>
 						Relevant details presented cleanly (text stacktraces, formatted
 						markdown, or screenshots. Please don’t paste large blocks of code
-						unformatted or post photos of your screen from your phone)
+						unformatted or post photos of your screen from your phone).
 					</li>
 					<li>
 						The specific question you have or the specific type of help
-						you&#39;re seeking
+						you&#39;re seeking.
 					</li>
 				</ul>
 			</ul>
@@ -294,16 +290,16 @@ export default function CodeOfConduct() {
 				services. If you are receiving unwelcome direct messages, please notify
 				an admin.
 			</p>
-			<br/>
+			<br />
 			<p>
 				<strong>
 					6. Practice good Discord hygiene by using threads for discussions and
-					emojis for light reactions.
+					emojis for light reactions
 				</strong>
 			</p>
 			<ul>
 				<li>
-					The medium is the message. Discord can foster a warm, collaborative,
+					The medium is the message. Discord can foster a welcoming, collaborative,
 					and organized community when used effectively. We want to respect
 					people’s attentions (thread notifications &gt; channel notifications
 					&gt; DM notifications) and we want to improve information density (a
