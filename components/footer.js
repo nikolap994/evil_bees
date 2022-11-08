4d273556af521427b73dd23eaed683f2fa77e240
Marija Cehic <cehicmarija@gmail.com>
2022-11-09 00:01:19 +0100

    Remove Footer bottom margin

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Footer() {
 	return (
-		<footer className="relative z-50 bg-indigo-900 mt-auto mb-12">
+		<footer className="relative z-50 bg-indigo-900 mt-auto">
 			<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 				<div className="flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-between">
 					<div className="flex gap-x-6">
