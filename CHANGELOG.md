5b313e6ef54c5cd4658023bc9328a765911f4bde
Marija Cehic <cehicmarija@gmail.com>
2022-11-09 00:47:00 +0100

    Add ltr, rtl tailwind animations

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,13 +3,12 @@ All notable changes to this project will be documented in this file.
 
 ## [Unreleased]
 ### Added
-
 - Homepage Hero component
 - Bees Tiers component
 - Hard Truth component
 - Open Source FAQ component
 - Solving Problems component
 - What We Stand For component
-
+- ltr, rtl tailwind animations
 ### Changed
 - Split index into components
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
+		"./public/**/*.{png,svg}",
 		"./pages/**/*.{js,ts,jsx,tsx}",
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'geo-pattern': "url('/images/geometric-bg.png')",
+				'idea-pattern': "url('/images/analyse.png')",
+			},
+			animation: {
+				'ltr-linear-infinite': 'ltr-linear-infinite 100s linear infinite',
+				'rtl-linear-infinite': 'rtl-linear-infinite 100s linear infinite',
+			},
+			keyframes: {
+				'ltr-linear-infinite': {
+					'from': { 'background-position': '0 0' },
+					'to': { 'background-position': '400% 0%' },
+				},
+				'rtl-linear-infinite': {
+					'from': { 'background-position': '400% 0%' },
+					'to': { 'background-position': '0 0' },
+				},
+			},
+		},
 	},
 	plugins: [],
 };
