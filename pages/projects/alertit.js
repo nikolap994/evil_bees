63a8edbd6d5726dc19caf6d2a4d857ed52d6c63d
Nikola Pavlović <nikolap994@gmail.com>
2022-11-13 15:20:58 +0100

    Simplify site logic

---
+++ pages/projects/alertit.js
@@ -0,0 +1,47 @@
+export default function PostPage({ frontmatter, content }) {
+	return (
+		<div className="container m-auto px-6 md:px-12 xl:px-6">
+			<h1 id="monitoring-everything-that-matters-to-you-">
+				Monitoring everything that matters to you.
+			</h1>
+			<h2 id="website-observation">Website observation</h2>
+			<p>
+				Know right away if your website is offline! Reliable monitoring saves
+				you money by alerting you before any major problems arise.
+			</p>
+			<h2 id="monitoring-pings">Monitoring pings</h2>
+			<p>
+				Utilize one of the instruments that administrators use the most
+				frequently to verify the availability of network devices.
+			</p>
+			<h2 id="port-surveillance">Port surveillance</h2>
+			<p>
+				Is email service still available? What about the crucial database
+				server, for instance? Let&#39;s verify! Track any specified service that
+				is active on any port.
+			</p>
+			<h2 id="keyword-monitoring">Keyword monitoring</h2>
+			<p>
+				Check the presence of the specific text in the content of the site by
+				crawling it whenever you want.
+			</p>
+			<h2 id="provide-updates-on-incidents">Provide updates on incidents</h2>
+			<p>
+				To the subscribers of your Status page, send out status updates through
+				email or other services you support.
+			</p>
+			<h2 id="response-periods">Response periods</h2>
+			<p>View your response times as a graph to identify performance blips.</p>
+			<p>
+				There are times when things go wrong. Be informed! Even the strongest
+				among us have downtime. However, it&#39;s crucial to be aware of it
+				before clients are impacted!
+			</p>
+			<p>
+				Using status pages, you can inform your consumers about incidents. Be
+				open-minded. Customers should be informed of scheduled interruptions.
+				Show them how you are thriving to provide a 100% online service.
+			</p>
+		</div>
+	);
+}
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,56 +1,25 @@
-import fs from "fs";
-import matter from "gray-matter";
 import Image from "next/image";
 import Link from "next/link";
 
-function date_sort(a, b) {
-	return (
-		new Date(b.frontmatter.date).getTime() -
-		new Date(a.frontmatter.date).getTime()
-	);
-}
-
-export async function getStaticProps() {
-	const files = fs.readdirSync("content/projects");
-
-	let posts = files.map(fileName => {
-		const slug = fileName.replace(".md", "");
-		const readFile = fs.readFileSync(`content/projects/${fileName}`, "utf-8");
-		const { data: frontmatter } = matter(readFile);
-		return {
-			slug,
-			frontmatter,
-		};
-	});
-	posts = posts.sort(date_sort);
-	return {
-		props: {
-			posts,
-		},
-	};
-}
-
-export default function Projects({ posts }) {
+export default function Projects() {
 	return (
 		<div className="container m-auto px-6 md:px-12 xl:px-6">
 			<div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-4 md:p-0">
-				{posts.map(({ slug, frontmatter }) => (
-					<div
-						key={slug}
-						className="border border-gray-200 m-2 rounded-xl shadow-lg overflow-hidden flex flex-col"
-					>
-						<Link href={`/projects/${slug.toLowerCase()}`}>
-							<Image
-								width={650}
-								height={340}
-								alt={frontmatter.title}
-								src={frontmatter.socialImage}
-							/>
-							<p className="p-4">{frontmatter.description}</p>
-							<h1 className="p-4">{frontmatter.title}</h1>
-						</Link>
-					</div>
-				))}
+				<div
+					key="alertit"
+					className="border border-gray-200 m-2 rounded-xl shadow-lg overflow-hidden flex flex-col"
+				>
+					<Link href={`/projects/alertit`}>
+						<Image
+							width={650}
+							height={340}
+							alt="alertit"
+							src="/projects/alertit-logo.png"
+						/>
+						<p className="p-4">Monitoring everything that matters to you.</p>
+						<h1 className="p-4">Alertit</h1>
+					</Link>
+				</div>
 			</div>
 		</div>
 	);
