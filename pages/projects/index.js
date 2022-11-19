205b4e5062382f8251d8fe3a276da091e3873d4f
Nemanja Ristic <cehicmarija@gmail.com>
2022-11-19 13:00:57 +0100

    Update Projects responsive styles

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function Projects() {
 	return (
-		<div className="container m-auto px-6 md:px-12 xl:px-6">
-			<div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-4 md:p-0">
+		<div className="container m-auto mt-16 px-6">
+			<div className="grid grid-cols-1 md:grid-cols-3 p-4 md:p-0">
 				<div
 					key="alertit"
 					className="border border-gray-200 m-2 rounded-xl shadow-lg overflow-hidden flex flex-col"
@@ -16,8 +16,8 @@ export default function Projects() {
 							alt="alertit"
 							src="/projects/alertit-logo.png"
 						/>
+						<h1 className="p-4 pb-0">Alertit</h1>
 						<p className="p-4">Monitoring everything that matters to you.</p>
-						<h1 className="p-4">Alertit</h1>
 					</Link>
 				</div>
 			</div>
