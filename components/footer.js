c76ddb4da900cfae3db1e316da07e0d7d19c1b5e
Nemanja Ristic <cehicmarija@gmail.com>
2022-11-20 00:21:59 +0100

    Update Footer responsive styles

--- components/footer.js
+++ components/footer.js
@@ -5,12 +5,12 @@ export default function Footer() {
 	return (
 		<footer className="bottom-0 z-50 bg-indigo-900 mt-auto">
 			<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-				<div className="lg:flex lg:flex-row items-center py-5 sm:flex-row sm:justify-between">
-					<p className="text-center lg:text-left lg:max-w-[400px] flex text-base leading-6 text-white">
+				<div className="md:flex md:flex-row items-center py-5 sm:flex-row sm:justify-between">
+					<p className="text-center lg:text-left md:max-w-[400px] flex text-base leading-6 text-white">
 						A constructive and inclusive community for software developers. With
 						you every step of your journey.
 					</p>
-					<div className="flex justify-between items-center">
+					<div className="flex justify-center md:justify-between gap-x-4 items-center">
 						<div className="flex mt-6 text-sm text-slate-500 sm:mt-0">
 							<Link href="/">
 								<Image
