5c361d4347e90afb4fab8e7eaad28e1dd268f0f4
Nikola Pavlović <nikolap994@gmail.com>
2022-11-19 12:33:32 +0100

    Fix missing strong

--- components/cta/what-we-stand-for.js
+++ components/cta/what-we-stand-for.js
@@ -8,9 +8,9 @@ export default function StandForCTA() {
         <p className="mt-4 text-lg tracking-tight text-white">
           We are not a web development organization, nor a marketing
           organization. We are a group of like-minded individuals who are
-          unified by a single idea — **create something great, learn & improve
-          ourselves along the way.** All of the projects and ideas we work on
-          are created using the **latest technologies,** and we look into every
+          unified by a single idea — <strong>create something great, learn & improve
+          ourselves along the way.</strong> All of the projects and ideas we work on
+          are created using the <strong>latest technologies,</strong> and we look into every
           aspect of the development before committing to a tech stack. We think
           that communication and planning is key, and don’t go into projects
           without thinking every aspect through. However, should things go awry,
