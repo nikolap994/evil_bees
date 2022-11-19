a7c890951f26f7ac059795d017a68de60e059ee0
Nikola Pavlović <nikolap994@gmail.com>
2022-11-19 15:44:00 +0100

    Remove cookie consent

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,3 @@
-import { useEffect } from "react";
-import { GoogleAnalytics } from "nextjs-google-analytics";
-import CookieConsent, {
-	getCookieConsentValue,
-	Cookies,
-} from "react-cookie-consent";
 import Head from "next/head";
 import { Analytics } from '@vercel/analytics/react';
 import Layout from "../components/layout";
@@ -11,23 +5,6 @@ import Layout from "../components/layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps: pageProps }) {
-	const handleAcceptCookie = () => {
-		<GoogleAnalytics trackPageViews />;
-	};
-
-	const handleDeclineCookie = () => {
-		Cookies.remove("_ga");
-		Cookies.remove("_gat");
-		Cookies.remove("_gid");
-	};
-
-	useEffect(() => {
-		const isConsent = getCookieConsentValue();
-		if (isConsent === "true") {
-			handleAcceptCookie();
-		}
-	}, []);
-
 	return (
 		<Layout>
 			<Head>
@@ -37,23 +14,6 @@ function MyApp({ Component, pageProps: pageProps }) {
 				/>
 			</Head>
 			<Component {...pageProps} />
-			<CookieConsent
-				enableDeclineButton
-				onAccept={handleAcceptCookie}
-				onDecline={handleDeclineCookie}
-				containerClasses="w-full fixed z-[123] flex justify-between px-12 py-4 border border-double bg-white color-black"
-				buttonClasses="bg-emerald-500 p-2 ml-3"
-				declineButtonClasses="bg-stone-900 text-white p-2"
-				contentClasses="py-2"
-				disableStyles={true}
-			>
-				This website uses cookies to enhance the user experience.
-			</CookieConsent>
-			{getCookieConsentValue() == "true" ? (
-				<GoogleAnalytics trackPageViews />
-			) : (
-				""
-			)}
 			<Analytics />
 		</Layout>
 	);
