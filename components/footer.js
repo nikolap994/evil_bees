57af7ee7f4c2bde0100c8e72dc9fb62820cb7403
Nemanja Ristic <cehicmarija@gmail.com>
2022-11-19 12:13:23 +0100

    Update Footer  background to black

--- components/footer.js
+++ components/footer.js
@@ -3,20 +3,20 @@ import Image from "next/image";
 
 export default function Footer() {
 	return (
-		<footer className="bottom-0 z-50 bg-indigo-900 mt-auto w-screen">
+		<footer className="bottom-0 z-50 bg-black mt-auto w-screen">
 			<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-				<div className="flex flex-col items-center border-t border-slate-400/10 py-5 sm:flex-row-reverse sm:justify-between">
+				<div className="flex flex-col items-center py-5 sm:flex-row-reverse sm:justify-between">
 					<div className="flex gap-x-6">
 						<Link href="https://github.com/Evil-Bees">
-							<Image width="80" height="80" alt="github" src="/github.png" />
+							<Image width="50" height="50" alt="github" src="/github.png" />
 						</Link>
 					</div>
 					<div className="flex mt-6 text-sm text-slate-500 sm:mt-0">
 						<Link href="/">
 							<Image
 								priority={true}
-								width="100"
-								height="100"
+								width="80"
+								height="50"
 								alt="evil-bees"
 								src="/evil-bees.png"
 							/>
@@ -41,7 +41,7 @@ export default function Footer() {
 						</Link>
 					</div>
 
-					<p className="flex text-base leading-6 text-white">
+					<p className="max-w-[400px] flex text-base leading-6 text-white">
 						A constructive and inclusive community for software developers. With
 						you every step of your journey.
 					</p>
