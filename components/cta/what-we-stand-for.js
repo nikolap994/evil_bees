3d86c24a3049a7e23f2f9d75c80ddec0fd86e8b1
Nikola Pavlović <nikolap994@gmail.com>
2022-11-20 10:44:54 +0100

    Disable gif animation

--- components/cta/what-we-stand-for.js
+++ components/cta/what-we-stand-for.js
@@ -1,18 +1,18 @@
 export default function StandForCTA() {
   return (
-    <div className="relative overflow-hidden py-32 bg-lines-pattern bg-repeat animate-rtl-linear-infinite">
+    <div className="relative overflow-hidden bg-indigo-900 py-32">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative">
         <h2 className="font-display mb-8 text-3xl tracking-tight text-white sm:text-4xl">
           What We Stand For ?
         </h2>
-        <p className="mt-4 text-lg tracking-tight bg-black-opacity p-4 text-white">
+        <p className="mt-4 text-lg tracking-tight p-4 text-white">
           We are not a web development organization, nor a marketing
           organization. We are a group of like-minded individuals who are
           unified by a single idea — <strong>create something great, learn & improve
           ourselves along the way.</strong> All of the projects and ideas we work on
           are created using the <strong>latest technologies,</strong> and we look into every
           aspect of the development before committing to a tech stack. We think
-          that communication and planning is key, and don’t go into projects
+          that communication and planning is key, and don&apos;t go into projects
           without thinking every aspect through. However, should things go awry,
           there will be a contingency plan set in place, as well as supportive
           community for you to lean on, and bounce ideas off of.
