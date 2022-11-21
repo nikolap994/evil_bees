aa7c2a3c572b4d618b021a281bfa85585d832ac1
Cehicmm <cehicmarija@gmail.com>
2022-11-21 23:13:32 +0100

    Update Projects max width, text bg color

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -8,21 +8,23 @@ export default function Projects() {
 			<Head>
 				<title>Evil Bees Projects</title>
 			</Head>
-			<div className="container m-auto my-16 px-6">
+			<div className="max-w-7xl m-auto my-16">
 				<div className="grid grid-cols-1 md:grid-cols-3 p-4 md:p-0">
 					<div
 						key="alertit"
-						className="border border-gray-200 m-2 rounded-xl shadow-lg overflow-hidden flex flex-col"
+						className="border border-gray-200 m-2 mb-6 rounded-xl shadow-lg overflow-hidden flex flex-col"
 					>
 						<Link href={`/projects/alertit`}>
 							<Image
 								width={650}
 								height={340}
-								alt="alertit"
+								alt="alertit logo"
 								src="/projects/alertit-logo.png"
 							/>
-							<h1 className="p-4 pb-0">Alertit</h1>
-							<p className="p-4">Monitoring everything that matters to you.</p>
+							<div className="bg-white">
+								<h1 className="p-4 pb-0">Alertit</h1>
+								<p className="p-4">Monitoring everything that matters to you.</p>
+							</div>
 						</Link>
 					</div>
 				</div>
