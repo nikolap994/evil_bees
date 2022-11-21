0a0b423ef6d0932dde62bde5c95c7d40ece3173f
Nikola Pavlović <nikolap994@gmail.com>
2022-11-21 18:47:44 +0100

    Add locale

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps: pageProps }) {
 			<Head>
 				<meta
 					name="viewport"
-					content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+					content="width=device-width"
 				/>
 			</Head>
 			<Component {...pageProps} />
