1916cc765485b8397a4d94f4c2119952494f0c46
Cehicmm <cehicmarija@gmail.com>
2022-11-21 23:08:01 +0100

    Update footer margins

--- components/footer.js
+++ components/footer.js
@@ -5,13 +5,13 @@ export default function Footer() {
 	return (
 		<footer className="bottom-0 z-50 bg-indigo-900 mt-auto">
 			<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-				<div className="md:flex md:flex-row items-center py-5 sm:flex-row sm:justify-between">
+				<div className="md:flex md:flex-row items-center pt-10 pb-5 md:pt-5 sm:flex-row sm:justify-between">
 					<p className="text-center lg:text-left md:max-w-[400px] flex text-base leading-6 text-white">
 						A constructive and inclusive community for software developers. With
 						you every step of your journey.
 					</p>
-					<div className="flex justify-center md:justify-between gap-x-4 items-center">
-						<div className="flex mt-6 text-sm text-slate-500 sm:mt-0">
+					<div className="mt-3 flex justify-center md:justify-between gap-y-4 gap-x-8 items-center">
+						<div className="flex text-sm text-slate-500 sm:mt-0">
 							<Link href="/">
 								<Image
 									priority={true}
