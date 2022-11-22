fcca897f5d4dccdab49af81b8558583d1d9ca0b3
Nikola Pavlović <nikolap994@gmail.com>
2022-11-22 12:09:10 +0100

    Revert config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
-const cspPolicy = "default-src 'self';connect-src  'self','preview.contentful.com','*.flippenterprise.net', 'aq.flippenterprise.net','https://www.google-analytics.com','dpm.demdex.net','stats.g.doubleclick.net','lcljoefresh.sc.omtrdc.net','sfml.flippback.com','p.flipp.com','https://sentry.io/', 'https://vitals.vercel-insights.com/'";
 const securityHeaders = [
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=63072000; includeSubDomains; preload'
-  },
-  {
-    key: 'X-Frame-Options',
-    value: 'SAMEORIGIN'
-  },
-  {
-    key: 'Content-Security-Policy',
-    value: cspPolicy
-  }
+	{
+		key: "X-XSS-Protection",
+		value: "1; mode=block",
+	},
 ];
 
 /** @type {import('next').NextConfig} */
