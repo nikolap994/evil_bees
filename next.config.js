51e3638fc16b4b7045a989333bb0d6c78f3a6ae0
Nikola Pavlović <nikolap994@gmail.com>
2022-11-22 12:05:25 +0100

    Revert mistake

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
+<<<<<<< HEAD
 const nextSafe = require("next-safe");
+=======
+const securityHeaders = [
+	{
+		key: "X-XSS-Protection",
+		value: "1; mode=block",
+	},
+];
+>>>>>>> parent of 44f1f03 (Add next safe)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -12,7 +21,11 @@ const nextConfig = {
 		return [
 			{
 				source: "/:path*",
+<<<<<<< HEAD
 				headers: nextSafe(true),
+=======
+				headers: securityHeaders,
+>>>>>>> parent of 44f1f03 (Add next safe)
 			},
 		];
 	},
