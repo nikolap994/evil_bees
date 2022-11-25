7a412096957b62e2f41b787345d84dd1a11c3b3a
Cehicmm <cehicmarija@gmail.com>
2022-11-25 01:43:27 +0100

    Update project styles

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -9,13 +9,14 @@ export default function Projects() {
 				<title>Evil Bees Projects</title>
 			</Head>
 			<div className="max-w-7xl m-auto my-16">
-				<div className="grid grid-cols-1 md:grid-cols-3 p-4 md:p-0">
+				<div className="grid grid-cols-1 md:grid-cols-2 p-4 md:p-0">
 					<div
 						key="alertit"
 						className="border border-gray-200 m-2 mb-6 rounded-xl shadow-lg overflow-hidden flex flex-col"
 					>
 						<Link href={`/projects/alertit`}>
 							<Image
+								className="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 overflow-hidden"
 								width={650}
 								height={340}
 								alt="alertit logo"
@@ -23,7 +24,9 @@ export default function Projects() {
 							/>
 							<div className="bg-white">
 								<h1 className="p-4 pb-0">Alertit</h1>
-								<p className="p-4">Monitoring everything that matters to you.</p>
+								<p className="p-4">
+									Monitoring everything that matters to you.
+								</p>
 							</div>
 						</Link>
 					</div>
