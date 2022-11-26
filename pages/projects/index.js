e25a77cf461e02af0a53be70808b26dadf7b2593
Cehicmm <cehicmarija@gmail.com>
2022-11-26 02:47:24 +0100

    Update projects design, add snap scroll

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -6,36 +6,44 @@ export default function Projects() {
 	return (
 		<>
 			<Head>
-				<title>Evil Bees Projects</title>
+				<title>Projects</title>
 			</Head>
-			<div className="max-w-7xl m-auto my-16">
-				<div className="grid grid-cols-1 md:grid-cols-2 p-4 md:p-0">
-					<div
-						key="alertit"
-						className="border border-gray-200 m-2 mb-6 rounded-xl shadow-lg overflow-hidden flex flex-col"
-					>
-						<Link href={`/projects/alertit`}>
-							<Image
-								className="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 overflow-hidden"
-								width={650}
-								height={340}
-								sizes="(max-width: 768px) 100vw,
-										(max-width: 1200px) 50vw,
-										30vw"
-								alt="alertit logo"
-								src="/projects/alertit-logo.png"
-								priority={true}
-							/>
-							<div className="bg-white">
-								<h1 className="p-4 pb-0">Alertit</h1>
-								<p className="p-4">
-									Monitoring everything that matters to you.
-								</p>
-							</div>
-						</Link>
-					</div>
+			<section className="py-16 bg-violet-100">
+				<div className="max-w-7xl mx-auto pb-8 max-w-[510px] text-center">
+					<h2 className="text-dark mb-6 font-bold text-6xl md:text-[40px]">
+						Our Projects
+					</h2>
+					<p className="text-body-color text-base">
+						We take great pride, and put our hearts and soul into each project.
+						Take a look at a few we're particularly proud of.
+					</p>
 				</div>
-			</div>
+				<div className="p-4 md:p-0">
+					<Link href={`/projects/alertit`}>
+						<div className="snap-x mx-auto snap-mandatory flex w-full overflow-scroll">
+							<div className="snap-start w-[80%] flex-shrink-0 flex items-center justify-center">
+								<Image
+									className="w-full"
+									width={2400}
+									height={340}
+									alt="alertit logo"
+									src="/projects/alertit-banner.png"
+									priority={true}
+								/>
+							</div>
+							<div className="snap-start bg-amber-200 w-[80%] flex-shrink-0  flex items-center justify-center text-xl p-12">
+								Monitoring everything that matters to you.
+							</div>
+							<div className="snap-start bg-indigo-200 w-[80%] flex-shrink-0 flex items-center justify-center text-xl  p-12">
+								Alertit is a tool for monitoring...
+							</div>
+							<div className="snap-start bg-gray-200 w-[80%] flex-shrink-0 flex items-center justify-center text-xl  p-12">
+								Techstack ??
+							</div>
+						</div>
+					</Link>
+				</div>
+			</section>
 		</>
 	);
 }
