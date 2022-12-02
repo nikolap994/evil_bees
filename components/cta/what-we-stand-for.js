eb96f0fee7d7dafdf273a5c41dde7d8e1c8342fe
Cehicmm <cehicmarija@gmail.com>
2022-12-02 01:12:24 +0100

    Add Bouncing octopus to What We Stand for

--- components/cta/what-we-stand-for.js
+++ components/cta/what-we-stand-for.js
@@ -1,6 +1,13 @@
+import Image from "next/image";
+import Octopus from "../../public/images/8.png";
+
 export default function StandForCTA() {
 	return (
-		<section className="bg-gradient-to-t from-indigo-900 to-indigo-200 text-white py-16 md:py-24 lg:py-32 px-8 lg:px-12">
+		<section className="relative bg-gradient-to-t from-indigo-900 to-indigo-200 text-white py-16 md:py-24 lg:py-32 px-8 lg:px-12">
+			<Image
+				className="md:animate-bounce absolute -top-[80px] left-[100px] md:top-[90px] md:left-[50%] lg:top-[40px] lg:left-[10%] w-[150px]"
+				src={Octopus}
+			/>
 			<div className="mx-auto max-w-7xl p-4 md:p-8">
 				<h2 className="font-display mb-8 text-3xl tracking-tight sm:text-4xl">
 					What We Stand For
