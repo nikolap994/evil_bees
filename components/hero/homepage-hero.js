20adce72bba36c211b79a64be9f156a59488a9ce
Cehicmm <cehicmarija@gmail.com>
2022-12-22 00:18:29 +0100

    Update Homepage Hero text, styles

--- components/hero/homepage-hero.js
+++ components/hero/homepage-hero.js
@@ -14,10 +14,10 @@ export default function HomepageHero() {
 			</div>
 			<div className="mx-auto max-w-7xl text-center py-16 md:py-24 lg:py-32 px-8 lg:px-12">
 				<h1 className="z-10 max-w-4xl font-display text-5xl font-medium tracking-tight text-left text-slate-900 md:text-7xl">
-					Lack the drive to level up as a developer? You’re in the right place!
+					Want to level up as a developer?
 				</h1>
-				<p className="p-4 mx-auto lg:mx-0 mt-16 max-w-2xl text-2xl tracking-tight text-left">
-					Make learning, levelling up fun and interactive!
+				<p className="mx-auto lg:mx-0 mt-16 lg:max-w-2xl text-6xl tracking-tight text-left">
+					You’re in the right place!
 				</p>
 			</div>
 		</section>
