5c906ddd0fc444df01c151bfb072ada0dbe9d49d
Cehicmm <cehicmarija@gmail.com>
2022-12-22 00:18:52 +0100

    Update Hard Truth text, styles

--- components/cta/hard-truth.js
+++ components/cta/hard-truth.js
@@ -1,36 +1,38 @@
-import Growth from "./growth";
+import Link from "next/link";
 
 export default function HardTruthCTA() {
 	return (
-		<section className="bg-violet-100">
-			<div className="text-black text-white py-16 md:py-24 lg:py-32 px-8 lg:px-12">
-				<div className="mx-auto max-w-7xl">
-					<div className="bg-white p-4 md:p-8 md:mx-auto md:text-center">
-						<h2 className="font-display text-4xl md:text-5xl tracking-tight mt-16">
-							Hard truth: Videos <span className="font-medium">don&apos;t</span>{" "}
-							teach you how to code.
-						</h2>
-						<p className="mt-8 text-lg tracking-tight text-justify lg:text-center max-w-4xl mx-auto">
-							Let&apos;s be honest. All you&apos;re doing while watching a
-							tutorial is blindly copying someone else&apos;s code - with no
-							reason or rhyme. You&apos;re not developing critical thinking, or
-							any actual skills that will help you grow as a developer.
-						</p>
+		<section className="">
+			<div className="text-white py-16 md:py-24 lg:py-32 mx-auto max-w-7xl">
+				<div className="text-slate-900 py-8 px-4 md:p-8 px-12 md:mx-auto md:text-center max-w-4xl text-">
+					<h2 className="text-4xl md:text-5xl tracking-tight mt-16">
+						Hard truth: Videos <span className="font-medium">don&apos;t</span>{" "}
+						teach you how to code.
+					</h2>
+					<p className="mt-8 text-lg tracking-tight text-justify lg:text-center max-w-4xl mx-auto">
+						At best, you'll learn to retype someone else's code —{" "}
+						<span className="italic">really really fast</span>. At worst, you'll
+						spend months pointlessly looking at the screen, wasting precious
+						time.
+					</p>
 
-						<h2 className="mt-16 font-display text-4xl md:text-5xl tracking-tight">
-							Solution
-						</h2>
-						<p className="mt-8 text-lg tracking-tight mb-16 text-justify lg:text-center max-w-4xl mx-auto">
-							Evil Bees is all about project-based learning. This means that
-							you&apos;ll get to work on cool ideas by yourself, and manage to
-							learn a lot along the way. But, we won&apos;t leave you stranded.
-							We pride ourselves on being a space for the community - by the
-							community.
-						</p>
-					</div>
+					<h2 className="mt-16 font-display text-4xl md:text-5xl tracking-tight">
+						So what should you do?
+					</h2>
+					<p className="my-8 text-lg tracking-tight md:mb-16 text-justify lg:text-center max-w-4xl mx-auto">
+						The best way to learn is by doing — and we offer just that! We
+						create open source projects that everyone is welcome to contribute
+						to. This way, you'll be able to learn the skills you need to become
+						a developer.
+					</p>
+					<Link
+						className="bg-indigo-400 py-3 px-10 text-white mt-6"
+						href="https://github.com/Evil-Bees"
+					>
+						Join us!
+					</Link>
 				</div>
 			</div>
-			<Growth />
 		</section>
 	);
 }
