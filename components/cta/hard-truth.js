e9b04f841ac3face7f53abcacf3a11f04034182a
Cehicmm <cehicmarija@gmail.com>
2022-12-22 02:09:18 +0100

    Update Hard Truth padding

--- components/cta/hard-truth.js
+++ components/cta/hard-truth.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function HardTruthCTA() {
 	return (
 		<section className="border-b border-white">
-			<div className="py-16 md:py-24 lg:py-32 mx-auto max-w-7xl">
+			<div className="py-16 px-8 md:py-24 lg:py-32 mx-auto max-w-7xl">
 				<div className="py-8 px-4 md:p-8 px-12 md:mx-auto md:text-center max-w-4xl text-">
 					<h2 className="text-4xl md:text-5xl tracking-tight">
 						Hard truth: Videos <span className="font-medium">don&apos;t</span>{" "}
