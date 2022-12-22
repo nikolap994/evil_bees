e64e7c3c63dba214acefb2b3dd8447e1e26cbd4c
Cehicmm <cehicmarija@gmail.com>
2022-12-23 00:18:36 +0100

    Update Git Button to open in new tab

--- components/ButtonGit.js
+++ components/ButtonGit.js
@@ -6,6 +6,7 @@ function ButtonGit({ text, linkTo }) {
 		<Link
 			className="flex items-center gap-4 bg-github-black text-white justify-center py-4 px-6 min-w-[250px] rounded-md hover:animate-pulse"
 			href={linkTo}
+			target="_blank"
 		>
 			{text}
 			<VscGithubInverted className="w-6 h-6" />{" "}
