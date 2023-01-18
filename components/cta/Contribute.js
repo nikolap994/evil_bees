17a2a921d170051584718b05abfdf0e1c99f2bf0
Cehicmm <cehicmarija@gmail.com>
2023-01-18 21:52:55 +0100

    Change Contribute bg to white

--- components/cta/Contribute.js
+++ components/cta/Contribute.js
@@ -5,9 +5,9 @@ import ButtonPurple from "../ButtonPurple";
 
 function Contribute() {
 	return (
-		<section className="py-16 lg:py-24 px-8 lg:px-12 border-b border-white text-lg">
+		<section className="bg-white text-black py-16 lg:py-24 px-8 lg:px-12 border-b border-white text-lg">
 			<div className="max-w-2xl mx-auto md:p-y8 lg:p-0">
-				<h2 className="font-display text-4xl md:text-5xl tracking-tight text-center mb-12">
+				<h2 className="font-display text-4xl md:text-5xl tracking-tight lg:text-center mb-12">
 					Start Contributing!
 				</h2>
 				<div>
@@ -30,20 +30,6 @@ function Contribute() {
 							/>
 						</li>
 					</ul>
-				</div>
-				<div className="md:px-4">
-					<h3 className="text-3xl text-left mb-7">How We Do It</h3>
-					<p className="text-justify my-8 lg:mt-2 lg:mb-0 text-md">
-						All of the projects and ideas we work on are created using the
-						<span className="font-bold"> latest technologies</span>, and we look
-						into every aspect of the development before committing to a tech
-						stack.
-					</p>
-					<p className="mt-4">
-						However, should things go awry, there will be a contingency plan set
-						in place, as well as supportive community for you to lean on, and
-						bounce ideas off of.
-					</p>
 				</div>
 			</div>
 		</section>
