af6209ae7353fa999eb79311c4ad2d10f72eed41
Cehicmm <cehicmarija@gmail.com>
2023-01-18 22:55:08 +0100

    Update ProjectsShort element widths

--- components/cta/ProjectShort.js
+++ components/cta/ProjectShort.js
@@ -11,23 +11,23 @@ function ProjectShort() {
 				return (
 					<section className="mx-auto xl:max-w-7xl mb-16" key={key}>
 						<div
-							className={`flex flex-col md:items-center gap-4 lg:gap-10 my-8 xl:my-16 ${
+							className={`mx-auto flex flex-col md:items-center lg:justify-around gap-4 lg:gap-10 my-8 xl:my-16 ${
 								key % 2 == 0 ? "lg:flex-row-reverse" : "lg:flex-row"
 							}`}
 						>
 							<h3 className="text-3xl mt-4 md:my-0 lg:hidden">
 								{project.name}
 							</h3>
-							<div className="md:w-full">
+							<div className="md:w-1/2">
 								<Image
-									className="rounded-lg lg:w-[1000px] lg:h-auto object-cover md:w-3/4 lg:w-full mx-auto"
+									className="rounded-lg lg:h-auto object-cover lg:w-[500px] xl:w-full mx-auto"
 									width="470"
 									height="200"
 									src={project.photo}
 									alt={project.name}
 								/>
 							</div>
-							<div className="flex flex-col justify-around lg:justify-center pb-4 gap-4 w-full md:max-w-[400px]">
+							<div className="flex flex-col lg:justify-center pb-4 gap-4 w-full md:max-w-[400px]">
 								<h3 className="text-3xl mt-4 md:my-0 hidden lg:block">
 									{project.name}
 								</h3>
