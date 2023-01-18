501a4de63a63cc17e4a459595b323a34c959efc2
Cehicmm <cehicmarija@gmail.com>
2023-01-18 21:59:14 +0100

    Decrease Purple button width

--- components/ButtonPurple.js
+++ components/ButtonPurple.js
@@ -4,7 +4,7 @@ import { FaDiscord } from "react-icons/fa";
 function ButtonPurple({ text, linkTo }) {
 	return (
 		<Link
-			className="flex items-center gap-4 bg-discord-purple text-white justify-center py py-4 px-6 min-w-[250px] rounded-md hover:animate-pulse"
+			className="flex items-center gap-4 bg-discord-purple text-white justify-center py-3 px-2 w-[200px] mx-auto rounded-md hover:animate-pulse"
 			href={linkTo}
 		>
 			{text} <FaDiscord className="w-6 h-6" />
